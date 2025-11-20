17467598cda3d58a4b58d229b52adb6dd407d4d1
subhadarshini-07 <subhadarshini.sahoo@infosys.com>
2025-11-20 19:46:25 +0530

    Update Transaction.java

--- Rewards-Project/src/main/java/Transaction.java
+++ Rewards-Project/src/main/java/Transaction.java
@@ -1,1 +1,25 @@
+import java.time.LocalDate;
 
+public class Transaction {
+    private Long id;
+    private Long customerId;
+    private double amount;
+    private LocalDate date;
+
+    // Getters, setters, constructors
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public Long getCustomerId() { return customerId; }
+    public void setCustomerId(Long customerId) { this.customerId = customerId; }
+    public double getAmount() { return amount; }
+    public void setAmount(double amount) { this.amount = amount; }
+    public LocalDate getDate() { return date; }
+    public void setDate(LocalDate date) { this.date = date; }
+
+    public Transaction(Long id, Long customerId, double amount, LocalDate date) {
+        this.id = id;
+        this.customerId = customerId;
+        this.amount = amount;
+        this.date = date;
+    }
+}
