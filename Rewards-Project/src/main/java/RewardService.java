012f30f01f620d06867a3061c27758dcd1a7cd07
subhadarshini-07 <subhadarshini.sahoo@infosys.com>
2025-11-20 19:45:44 +0530

    Update and rename Customer.java to RewardService.java

---
+++ Rewards-Project/src/main/java/RewardService.java
@@ -0,0 +1,15 @@
+import org.springframework.stereotype.Service;
+
+@Service
+public class RewardService {
+    public int calculatePoints(double amount) {
+        int points = 0;
+        if (amount > 100) {
+            points += (int) ((amount - 100) * 2);
+            points += (int) (50 * 1); // 1 point for dollars between $50 and $100
+        } else if (amount > 50) {
+            points += (int) ((amount - 50) * 1);
+        }
+        return points;
+    }
+}
