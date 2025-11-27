d3370ea96f1f01cad37979f59e176a2991ca64d0
subhadarshini-07 <subhadarshini.sahoo@infosys.com>
2025-11-27 20:22:34 +0530

    Create RewardApplication.java

---
+++ Rewards-Project/src/main/java/Service/RewardApplication.java
@@ -0,0 +1,19 @@
+package com.offer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class RewardsApplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        SpringApplication.run(RewardsApplication.class, args);
+    }
+}
