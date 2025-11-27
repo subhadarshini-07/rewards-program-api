cd73fb24e72f104ece37e504dd769aa9595a1e6d
subhadarshini-07 <subhadarshini.sahoo@infosys.com>
2025-11-27 20:19:02 +0530

    Update Customer.java

--- Rewards-Project/src/main/java/model/Customer.java
+++ Rewards-Project/src/main/java/model/Customer.java
@@ -1,1 +1,48 @@
 
+package com.offer.model;
+
+public class Customer {
+	
+	private String id;
+	private String name;
+	private String email;
+	
+	
+	public Customer(String id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", email=" + email + "]";
+	}
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
+
