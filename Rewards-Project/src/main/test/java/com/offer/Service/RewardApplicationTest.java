b7938a91387b6fcf9245fd9b5c5da1f9dadbe861
subhadarshini-07 <subhadarshini.sahoo@infosys.com>
2025-11-27 20:27:54 +0530

    Create RewardApplicationTest.java

---
+++ Rewards-Project/src/main/test/java/com/offer/Service/RewardApplicationTest.java
@@ -0,0 +1,17 @@
+package com.offer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Unit test for simple App.
+ */
+@SpringBootTest
+public class RewardsApplicationTest {
+	
+	@Test
+	void contextLoads() {
+		// Test passes if the Spring Application Context loads successfully.
+	}
+ 
+}
